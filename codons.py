8e8d777fd381779497de372108b5b3090ccd3cc8
R-salomon <ronniesalomon8@gmail.com>
2024-12-11 22:22:41 +0200

    amen1

--- codons.py
+++ codons.py
@@ -1,14 +1,14 @@
 def create_codon_dict(file_path):
-  rows = []
-  file = open(path, "r")
-  rows = file.readlines()
-  file.close()
-
-  Codon_to_Amino_Acid = {}
-  for row in rows:
-    row_cells = row.strip().split('\t')
-    codon = row_cells[0]
-    amino_acid = row_cells[2]
-    Codon_to_Amino_Acid[codon] = amino_acid
-  return Codon_to_Amino_Acid
+    rows = []
+    file = open(path, "r")
+    rows = file.readlines()
+    file.close()
+  
+    Codon_to_Amino_Acid = {}
+    for row in rows:
+      row_cells = row.strip().split('\t')
+      codon = row_cells[0]
+      amino_acid = row_cells[2]
+      Codon_to_Amino_Acid[codon] = amino_acid
+    return Codon_to_Amino_Acid
 
