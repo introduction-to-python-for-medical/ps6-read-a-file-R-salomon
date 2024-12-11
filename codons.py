35e9e43907d01bc2a6e88205fc9d1cb0bb18b93a
R-salomon <ronniesalomon8@gmail.com>
2024-12-11 22:19:07 +0200

    amen

--- codons.py
+++ codons.py
@@ -1,4 +1,14 @@
 def create_codon_dict(file_path):
-    pass # Replace the pass with your code
+  rows = []
+  file = open(path, "r")
+  rows = file.readlines()
+  file.close()
 
+  Codon_to_Amino_Acid = {}
+  for row in rows:
+    row_cells = row.strip().split('\t')
+    codon = row_cells[0]
+    amino_acid = row_cells[2]
+    Codon_to_Amino_Acid[codon] = amino_acid
+  return Codon_to_Amino_Acid
 
